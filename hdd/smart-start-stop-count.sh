cb0620d636e86b280d803f9f784607d2de94078a
Wirawan Purwanto <wirawan0@gmail.com>
2014-11-27 18:41:05 -0500

    * Allow DEVICE and DELAY to be set externally.

--- hdd/smart-start-stop-count.sh
+++ hdd/smart-start-stop-count.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 #
 # smart-start-stop-count.sh
-# A SMART utility to check the start-stop count value of the disk
-# over time (the Load_Cycle_Count
+# A SMART-based utility to check the load/unload (start/stop) cycle value
+# of a disk over time (the Load_Cycle_Count parameter).
+#
+# NOTE: Requires root privilege to run the smartctl command.
 #
 # Created: 20120925
 # Wirawan Purwanto
 #
 
+: ${DEVICE:=/dev/sda}
+: ${DELAY:=1m}
+
 while true; do
   date "+%s  %Y-%m-%d %H:%M:%S  " | tr -d '\n'
-  smartctl -A /dev/sda | grep Load_Cycle_Count
-  sleep 1m
+  smartctl -A "$DEVICE" | grep Load_Cycle_Count
+  sleep "$DELAY"
 done
