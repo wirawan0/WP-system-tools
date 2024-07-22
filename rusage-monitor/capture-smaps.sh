211322e15a2bed942ebad24980dbe7a84812d668
Wirawan Purwanto <wirawan0@gmail.com>
2024-07-22 17:22:38 -0400

    * Allow smaps capture by specifying the PID.
      Last-mtime: 2023-05-04T10:32:01 -0400

--- rusage-monitor/capture-smaps.sh
+++ rusage-monitor/capture-smaps.sh
@@ -10,10 +10,25 @@
 
 TIMESTAMP=$(date +"%s-%Y%m%dT%H%M%S")
 
-SMAPS_FILE="$1"
+case "$1" in
+(--pid|-p)
+    PROC_PID=$1
+    SMAPS_FILE=/proc/$PROC_PID/smaps
+    SMAPS_OUT_FILE="smaps-$PID-$TIMESTAMP.txt"
+    ;;
+(-*)
+    echo "Invalid switch: $1" >&2
+    exit 1
+    ;;
+(*)
+    SMAPS_FILE="$1"
+    SMAPS_OUT_FILE="smaps-$TIMESTAMP.txt"
+    ;;
+esac
+
 if [ ! -f "$SMAPS_FILE" ]; then
     echo "Error: smaps file does not exist" >&2
     exit 2
 fi
 
-cp -p "$SMAPS_FILE" "smaps-$TIMESTAMP.txt"
+cp -p "$SMAPS_FILE" "$SMAPS_OUT_FILE"
